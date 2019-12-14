f8fa23cd6747c7ee8d9e4f5a223b500c963e8c95
natasha17 <17061990@inbox.ru>
2019-12-14 17:08:33 +0300

    addMedia

--- src/Blok6/Footer.js
+++ src/Blok6/Footer.js
@@ -7,15 +7,13 @@ import viber from './viber.png'
         function Footer() {
             return    <div className={styles.skill}>
                 <div className={styles.container}>
-                    <div>Контакты</div>
+                    <div>Герасимович Наталия</div>
                     <div className={styles.socialContainer}>
                         <div className={styles.socialIcons}> <img src={inst} alt=""/></div>
                         <div className={styles.socialIcons}><img src={em} alt=""/></div>
                         <div className={styles.socialIcons}><img src={what} alt=""/></div>
                         <div className={styles.socialIcons}><img src={viber} alt=""/></div>
                     </div>
-                    <span className={styles.title}>Контакты</span>
-                      <img src="" alt=""/>
                     <span className={styles.prava}>©2019 Все права защищены</span>
                 </div>
             </div>
