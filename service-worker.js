0e29a23bfc18a83052dc35dd0c52575348a4cd1d
natasha17 <17061990@inbox.ru>
2020-01-17 20:53:50 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.b047e0525f7784722c8c0d14a20388e5.js"
+  "/Portfolio/precache-manifest.f94b8f943fe8bb34fd4355360579a629.js"
 );
 
 self.addEventListener('message', (event) => {
