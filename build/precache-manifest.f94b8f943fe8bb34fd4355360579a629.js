a0c5de7b693a9957b952422d9f65471bafbd11bb
natasha17 <17061990@inbox.ru>
2020-01-17 20:55:14 +0300

    addcreateParticles

---
+++ build/precache-manifest.f94b8f943fe8bb34fd4355360579a629.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dfeff460e70dafc981eb99a2b95bd14",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "7c79c01d47dfa4b9cb9c",
+    "url": "/Portfolio/static/css/main.e316abca.chunk.css"
+  },
+  {
+    "revision": "680137bad93a5eb9d695",
+    "url": "/Portfolio/static/js/2.5c121eab.chunk.js"
+  },
+  {
+    "revision": "7c79c01d47dfa4b9cb9c",
+    "url": "/Portfolio/static/js/main.da0b46f1.chunk.js"
+  },
+  {
+    "revision": "efb536f25f40738a93c8",
+    "url": "/Portfolio/static/js/runtime-main.60e60d4b.js"
+  },
+  {
+    "revision": "34d2952f2e4ca7635cd79487558874f9",
+    "url": "/Portfolio/static/media/project_1.34d2952f.png"
+  },
+  {
+    "revision": "e00161a8953b87d08b830a15d6ca859b",
+    "url": "/Portfolio/static/media/toDolist2.2.e00161a8.png"
+  }
+]);
