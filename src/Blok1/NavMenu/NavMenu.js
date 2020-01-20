7c380477b4e0adefd66a7c057e7c20f0ccc143d6
natasha17 <17061990@inbox.ru>
2020-01-20 22:34:32 +0300

    react-scroll

--- src/Blok1/NavMenu/NavMenu.js
+++ src/Blok1/NavMenu/NavMenu.js
@@ -1,24 +1,55 @@
 import React from 'react';
 import styles from './NavMenu.module.css';
+import { Link, animateScroll as scroll } from "react-scroll";
 
-
-
-
-
-function NavMenu() {
-
+ class NavMenu extends React.Component {
+     scrollToTop = () => {
+         scroll.scrollToTop();
+     };
+    render() {
     return (
-
-            <div className={styles.navMenu}>
-                <a href="#main" className={styles.link}>Main</a>
-                <a href="#skills" className={styles.link}>Skills</a>
-                <a href="#project" className={styles.link}>Project</a>
-                <a href="#contact" className={styles.link}>Contact</a>
-
-            </div>
-
-
-
+        <div className={styles.navMenu}>
+            <a href="#main" className={styles.link} onClick={this.scrollToTop}>
+                <Link
+                    activeClass="active"
+                    to="main"
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={700}
+                >Main
+                </Link>
+            </a>
+            <a href="#skills" className={styles.link} onClick={this.scrollToTop}>
+                <Link
+                    activeClass="active"
+                    spy={true}
+                    to="skills"
+                    smooth={true}
+                    offset={-70}
+                    duration={700}
+                >Skills
+                </Link>
+            </a>
+            <a href="#project" className={styles.link} onClick={this.scrollToTop}>
+                <Link
+                    activeClass="active"
+                    to="project"
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={700}
+                >Project</Link></a>
+            <a href="#contact" className={styles.link} onClick={this.scrollToTop}>
+                <Link
+                    activeClass="active"
+                    to="contact"
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={700}
+                >Contact</Link></a>
+        </div>
     );
-}
+}}
 export default NavMenu;
