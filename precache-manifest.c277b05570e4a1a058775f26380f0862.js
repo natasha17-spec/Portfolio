eb297fa182be74e049216b448dfa0c6b88a77741
natasha17 <17061990@inbox.ru>
2020-01-27 23:15:23 +0300

    Updates

--- precache-manifest.c277b05570e4a1a058775f26380f0862.js
+++ precache-manifest.c277b05570e4a1a058775f26380f0862.js
@@ -1,23 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5dfeff460e70dafc981eb99a2b95bd14",
+    "revision": "9f605fcd28b94d4b36013a8a90c9c29e",
     "url": "/Portfolio/index.html"
   },
   {
-    "revision": "7c79c01d47dfa4b9cb9c",
-    "url": "/Portfolio/static/css/main.e316abca.chunk.css"
+    "revision": "53b9fcbd8bb16425731a",
+    "url": "/Portfolio/static/css/main.785e1efc.chunk.css"
   },
   {
-    "revision": "680137bad93a5eb9d695",
-    "url": "/Portfolio/static/js/2.5c121eab.chunk.js"
+    "revision": "8efdca0440c36d3232ac",
+    "url": "/Portfolio/static/js/2.6c654f04.chunk.js"
   },
   {
-    "revision": "7c79c01d47dfa4b9cb9c",
-    "url": "/Portfolio/static/js/main.da0b46f1.chunk.js"
+    "revision": "53b9fcbd8bb16425731a",
+    "url": "/Portfolio/static/js/main.8605f1cb.chunk.js"
   },
   {
     "revision": "efb536f25f40738a93c8",
     "url": "/Portfolio/static/js/runtime-main.60e60d4b.js"
+  },
+  {
+    "revision": "a7a3f4eb4a54616bef6e96e492c52d7f",
+    "url": "/Portfolio/static/media/photo.a7a3f4eb.jpg"
   },
   {
     "revision": "34d2952f2e4ca7635cd79487558874f9",
