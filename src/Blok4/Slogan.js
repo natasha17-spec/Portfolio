1059b2265b235f289764c2a0566c9cd48c86afa6
natasha17 <17061990@inbox.ru>
2020-01-28 21:11:35 +0300

    addAnimatedButtonContact

--- src/Blok4/Slogan.js
+++ src/Blok4/Slogan.js
@@ -8,10 +8,16 @@ function Slogan() {
         <div className={styles.skill}>
             <div className={styles.conteiner}>
                 <span className={styles.title}>I Am Available For Freelancer</span>
-                <button className={styles.btn}>Hire me</button>
+                <div className={styles.wrapper}>
+                    <a href={'#!'} className={styles.btn}> Hire me
+                        <svg>
+                            <rect></rect>
+                        </svg>
+                    </a>
+                </div>
             </div>
         </div>
-    );
+);
 }
 
 export default Slogan;
