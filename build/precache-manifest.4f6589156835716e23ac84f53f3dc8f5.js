6023ca80befe54ba1dc96d690b83a3feed8b8c5b
natasha17 <17061990@inbox.ru>
2020-01-28 22:27:50 +0300

    addAnimation

--- build/precache-manifest.4f6589156835716e23ac84f53f3dc8f5.js
+++ build/precache-manifest.4f6589156835716e23ac84f53f3dc8f5.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9f605fcd28b94d4b36013a8a90c9c29e",
+    "revision": "8d9e3bf19061c47df7349c2e7a667715",
     "url": "/Portfolio/index.html"
   },
   {
-    "revision": "53b9fcbd8bb16425731a",
-    "url": "/Portfolio/static/css/main.785e1efc.chunk.css"
+    "revision": "a304a12444eebf3964ba",
+    "url": "/Portfolio/static/css/main.8ece1fc0.chunk.css"
   },
   {
-    "revision": "8efdca0440c36d3232ac",
-    "url": "/Portfolio/static/js/2.6c654f04.chunk.js"
+    "revision": "71490abbc94bcf369e51",
+    "url": "/Portfolio/static/js/2.7ec76de7.chunk.js"
   },
   {
-    "revision": "53b9fcbd8bb16425731a",
-    "url": "/Portfolio/static/js/main.8605f1cb.chunk.js"
+    "revision": "a304a12444eebf3964ba",
+    "url": "/Portfolio/static/js/main.d22d4f0a.chunk.js"
   },
   {
     "revision": "efb536f25f40738a93c8",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.c277b05570e4a1a058775f26380f0862.js"
+  "/Portfolio/precache-manifest.4f6589156835716e23ac84f53f3dc8f5.js"
 );
 
 self.addEventListener('message', (event) => {
