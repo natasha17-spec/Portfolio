47456f9b7b4f6dbdde54a26ff44e9af1d4eaddc3
natasha17 <17061990@inbox.ru>
2020-02-20 23:08:59 +0300

    Updates

---
+++ precache-manifest.6fda9f71678e301a5f9f5c91861bf0e6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc3e0ba9ecf9d35c85ca7496f79cae5e",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "7556dcc9442ea9a4a28f",
+    "url": "/Portfolio/static/css/main.d56b7f1b.chunk.css"
+  },
+  {
+    "revision": "8ac90b27dd79284cecfc",
+    "url": "/Portfolio/static/js/2.0befe8fc.chunk.js"
+  },
+  {
+    "revision": "7556dcc9442ea9a4a28f",
+    "url": "/Portfolio/static/js/main.7db8a418.chunk.js"
+  },
+  {
+    "revision": "b199106dff44bb25fd95",
+    "url": "/Portfolio/static/js/runtime-main.8af804fc.js"
+  },
+  {
+    "revision": "d57959b2f8e89ceca6f323a4d4dda170",
+    "url": "/Portfolio/static/media/1.d57959b2.png"
+  },
+  {
+    "revision": "88482a18b7ce7d1f7684c3e837163b03",
+    "url": "/Portfolio/static/media/cats.88482a18.jpg"
+  },
+  {
+    "revision": "a7a3f4eb4a54616bef6e96e492c52d7f",
+    "url": "/Portfolio/static/media/photo.a7a3f4eb.jpg"
+  },
+  {
+    "revision": "34d2952f2e4ca7635cd79487558874f9",
+    "url": "/Portfolio/static/media/project_1.34d2952f.png"
+  }
+]);
